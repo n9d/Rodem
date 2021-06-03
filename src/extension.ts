29dd4035e4b5c85ef7d07b7e04ab29dd33647d96
n9d <39367157+n9d@users.noreply.github.com>
2021-06-04 03:05:43 +0900

    eslint

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const code = extract();
 		if (code.lang!=="nop") {
 			const out = execute(code);
-			output(code,out)
+			output(code,out);
 		}
 	});
 
@@ -38,10 +38,10 @@ function output(code:Code, out:Out){
 
 		editor.edit(builder => {
 			const doc : vscode.TextDocument = editor.document;
-			if (code.output.length==2){
-				builder.replace(new vscode.Range(doc.lineAt(code.output[0]).range.start,doc.lineAt(code.output[1]).range.end), `\`\`\`output\n${out.stdout}\n\`\`\``)
+			if (code.output.length === 2){
+				builder.replace(new vscode.Range(doc.lineAt(code.output[0]).range.start,doc.lineAt(code.output[1]).range.end), `\`\`\`output\n${out.stdout}\n\`\`\``);
 			}else{
-				builder.insert(new vscode.Position(code.output[0], 0),`\n\`\`\`output\n${out.stdout}\n\`\`\`\n`)
+				builder.insert(new vscode.Position(code.output[0], 0),`\n\`\`\`output\n${out.stdout}\n\`\`\`\n`);
 			}
 		});
 
@@ -50,7 +50,7 @@ function output(code:Code, out:Out){
 
 // code実行時のエラー時の処理未実装
 function execute(code:Code):Out {
-	const lang = vscode.workspace.getConfiguration("rodem").lang[code.lang]
+	const lang = vscode.workspace.getConfiguration("rodem").lang[code.lang];
 
 	if (lang){
 		temp.track();
@@ -73,22 +73,22 @@ function extract():Code {
 		const txt = (doc.getText()+"\n```").split(newline);
 
 		// NOTE: あとでflatmapで書き換えること
-		const pre = txt.map((x,y)=>{return {text:x,line:y}})
-			.filter(x=>String(x.text).match(/^```.*$/))
+		const pre = txt.map((x,y)=>{return {text:x,line:y};})
+			.filter(x=>String(x.text).match(/^```.*$/));
 
 		const line = editor.selection.active.line;
 		const start = max_by(pre, x=>x.line>=line?0:x.line);
-		const end = min_by(pre, x=>x.line<=line?txt.length-1:x.line)
-		const lang = (start?.text.match(/```(.+)$/)||[])[1]
-		const next_start = min_by(pre, (x:any)=>x.line<=Number(end?.line)?txt.length-1:x.line)
-		const next_end = min_by(pre, (x:any)=>x.line<=Number(next_start?.line)?txt.length-1:x.line)
+		const end = min_by(pre, x=>x.line<=line?txt.length-1:x.line);
+		const lang = (start?.text.match(/```(.+)$/)||[])[1];
+		const nextStart = min_by(pre, (x:any)=>x.line<=Number(end?.line)?txt.length-1:x.line);
+		const nextEnd = min_by(pre, (x:any)=>x.line<=Number(nextStart?.line)?txt.length-1:x.line);
 
 		if (start && end && lang && lang!=="output") {
 			return {
 				lang: lang,
 				code: txt.slice(start.line+1,end.line).join(newline),
-				output: next_start?.text.match(/```output$/) && next_end?.text.match(/```/) && next_start.line<next_end.line? [next_start.line,next_end.line] : [end.line+1]
-			}
+				output: nextStart?.text.match(/```output$/) && nextEnd?.text.match(/```/) && nextStart.line<nextEnd.line? [nextStart.line,nextEnd.line] : [end.line+1]
+			};
 		}
 	}
 	return {lang:"nop",code:"",output:[]};
@@ -100,7 +100,7 @@ function max_by<T,U>(arr:T[], func:(arg:T)=>U):T{
     let [r, m] = [0, func(arr[0])];
     for(let i=0; i<arr.length; ++i) {
         let t = func(arr[i]);
-        [r, m] = t > m ? [i, t] : [r, m]
+        [r, m] = t > m ? [i, t] : [r, m];
     }
     return arr[r];
 }
@@ -109,7 +109,7 @@ function min_by<T,U>(arr:T[], func:(arg:T)=>U):T{
     let [r, m] = [0, func(arr[0])];
     for(let i=0; i<arr.length; ++i) {
         let t = func(arr[i]);
-        [r, m] = t < m ? [i, t] : [r, m]
+        [r, m] = t < m ? [i, t] : [r, m];
     }
     return arr[r];
 }
